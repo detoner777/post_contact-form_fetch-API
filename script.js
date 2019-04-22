8e7eb2088949743425a8489fffa691cc88cf2e52
Detoner <detoner777@gmail.com>
2019-04-22 07:32:54 +0300

    JS done

--- script.js
+++ script.js
@@ -1,5 +1,46 @@
+
+// https://www.youtube.com/watch?v=c3qWHnJJbSY (have enother example with URLSearchParam send method)
+
+const postData = document.getElementById('postData');
+
+    postData.addEventListener('submit', function (e) {
+        e.preventDefault();
+
+        const formData = new FormData(this);
+
+        fetch('form.php', {
+            method: 'post',
+            body: formData
+      }).then(function (response) {
+          return response.text();
+      }).then(function (text) {
+          console.log(text);
+      }).catch(function (error) {
+          console.error(error);
+      })
+    });
+
+
+
+/* Have e problems with code below
+
 document.getElementById('postData').addEventListener('submit', postData);
 
  function postData(event){
             event.preventDefault();
+
+            let firstName = document.getElementById('fname').value;
+            let secondName = document.getElementById('sname').value;
+
+            fetch('https://jsonplaceholder.typicode.com/posts', {
+                method: 'POST',
+                headers: new Headers(),
+                body:JSON.stringify({firstName:firstName, secondName:secondName})
+                })
+    .then((res) => res.json())
+    .then((data) => console.log(data))
+    .catch((err)) => console.log(err))
+                
+            }
+           */
 
